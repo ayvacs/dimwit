2209d384a29cc88fe8c0495022c82345c1f5fc75
dom <ayvacs@proton.me>
2024-03-20 19:28:34 -0400

    fix: console reset format bug

--- src/modules/print.ts
+++ src/modules/print.ts
@@ -32,22 +32,22 @@ module.exports = {
 
     // print (white)
     log: function(serviceName: string = "Global", text: string = "", doColors: boolean = true) {
-        console.log(`${doColors ? "\x1b[0m" : ""}  ${new Date().toLocaleString()}  |  ${serviceName}  |  ${text}${doColors ? "\x1b[0m" : ""}`);
+        console.log(`\x1b[0m  ${new Date().toLocaleString()}  |  ${serviceName}  |  ${text}${doColors ? "\x1b[0m" : ""}`);
     },
 
     // warn (yellow)
     warn: function(serviceName: string = "Global", text: string = "", doColors: boolean = true) {
-        console.warn(`${doColors ? "\x1b[0m\x1b[31m" : ""}? ${new Date().toLocaleString()}  |  ${serviceName}  |  ${text}${doColors ? "\x1b[0m" : ""}`);
+        console.warn(`\x1b[0m${doColors ? "\x1b[31m" : ""}? ${new Date().toLocaleString()}  |  ${serviceName}  |  ${text}${doColors ? "\x1b[0m" : ""}`);
     },
 
     // error (red)
     error: function(serviceName: string = "Global", text: string = "", doColors: boolean = true) {
-        console.error(`${doColors ? "\x1b[0m\x1b[33m" : ""}! ${new Date().toLocaleString()}  |  ${serviceName}  |  ${text}${doColors ? "\x1b[0m" : ""}`);
+        console.error(`\x1b[0m${doColors ? "\x1b[33m" : ""}! ${new Date().toLocaleString()}  |  ${serviceName}  |  ${text}${doColors ? "\x1b[0m" : ""}`);
     },
 
     // affirm (green)
     affirm: function(serviceName: string = "Global", text: string = "", doColors: boolean = true) {
-        console.error(`${doColors ? "\x1b[0m\x1b[32m" : ""}✓ ${new Date().toLocaleString()}  |  ${serviceName}  |  ${text}${doColors ? "\x1b[0m" : ""}`);
+        console.error(`\x1b[0m${doColors ? "\x1b[32m" : ""}✓ ${new Date().toLocaleString()}  |  ${serviceName}  |  ${text}${doColors ? "\x1b[0m" : ""}`);
     }
 
 }
